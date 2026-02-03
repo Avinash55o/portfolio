80797a57afd9d03f1af562eb7b3cf332f560d466
avinash55o <avinashboruah8@gmail.com>
2026-02-03 12:08:59 +0530

    updating the project data to fetch directly from the github

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,42 +1,5 @@
-export const  ProjectData = [
-    {   
-        id:1,
-        projectName : "EatoMoji",
-        description: "A fun food app where your mood picks your next snack or meal—just tap how you feel and get instant suggestions. Like a tiny foodie friend that says, “You’re stressed? Ok, here’s what we’re eating.”",
-        tech :["React","Node.js","Express.js","PostgreSQL","JWT","Tailwind CSS"],
-        link : "https://eato-moji.vercel.app/",
-        github : "https://github.com/Avinash55o/EatoMoji"
-    },
-    {   
-        id:2,
-        projectName : "SupportBot-AI",
-        description: "An AI-powered support system that lets users submit issues and automatically organizes them into clean, trackable tickets. Less chaos, more clarity—your basic “support team but smarter” setup.",
-        tech : ["Flask","JWT","SQLAlchemy","React 18","TypeScript","Vite","Tailwind CSS/Shadcn UI"],
-        link : "/not-available",
-        github : "https://github.com/Avinash55o/SupportBot-AI"
-    },
-      {   
-        id:3,
-        projectName : "PawSense",
-        description: "PawSense is an AI-powered dog breed detector where you upload a photo and instantly get smart breed guesses with helpful insights. Built with FastAPI, Next.js, PyTorch, and Tailwind for a fast, modern feel.",
-        tech : ["FastAPI","Next.js","Tailwind CSS","PyTorch","OpenVINO","BLIP"],
-        link : "https://pawsense-model-avinash.onrender.com/",
-        github : "https://github.com/Avinash55o/pawSense"
-    },
-       {   
-        id:4,
-        projectName : "Recipe App",
-        description: "Recipe-App is a simple cooking companion where users can browse recipes, save favorites, and explore dishes through a clean React + Tailwind interface, backed by a TypeScript Express API powered by Prisma.",
-        tech :["React (TS)","Express (TS)","Node (TS)","Prisma","Tailwind CSS"],
-        link : "/not-available",
-        github : "https://github.com/Avinash55o/Recipe-app"
-    },
-    {   
-        id:4,
-        projectName : "Kawaii Gallery",
-        description: "Kawaii Gallery is a cute, minimal web app where you can scroll through adorable artwork, search for your favorite styles, and enjoy a clean, pastel UI made with React, Vite, and Tailwind.",
-        tech : ["React","Vite","JavaScript","HTML","CSS","Tailwind CSS"],
-        link : "https://kawaiigallery.netlify.app/",
-        github : "https://github.com/Avinash55o/Kawaii-Gallery"
-    },
+export const ProjectName=[
+    "pawsense", "backkit", "Recipe-app", "slate","MiniTree", "Image-clssification-project", "EtoMoji"
 ]
+
+export const GitHUB_UserName= "Avinash55o"
